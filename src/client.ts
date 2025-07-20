124c53df84e01af7ae0bf6c2adb9ddc4e31961ff
yashjawale <yash25.j@gmail.com>
2025-07-20 22:48:12 +0530

    feat: add user interaction prompts and new user data to the client

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,7 @@
+import { input, select } from '@inquirer/prompts';
 import { Client } from '@modelcontextprotocol/sdk/client/index.js';
 import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js';
+import { Tool } from '@modelcontextprotocol/sdk/types.js';
 
 const mcp = new Client({
 	name: "Learn MCP Client",
@@ -23,6 +25,56 @@ async function main() {
 		mcp.listResources(),
 		mcp.listResourceTemplates()
 	])
+
+	console.log("You are connected")
+
+	while (true) {
+		const option = await select({
+			message: "What would you like to do?",
+			choices: [
+				"Query",
+				"Tools",
+				"Resources",
+				"Prompts"
+			]
+		})
+
+		switch (option) {
+			case "Tools":
+			const toolName = await select({
+				message: "Select a tool",
+				choices: tools.map(tool => ({
+					name: tool.annotations?.title || tool.name,
+					value: tool.name,
+					description: tool.description
+				}))
+			})
+			const tool = tools.find(t => t.name === toolName)
+			if (tool == null) {
+				console.error("Tool not found.")
+			} else {
+				await handleTool(tool)
+			}
+			break
+		}
+	}
+}
+
+async function handleTool(tool: Tool) {
+	const args: Record<string, string> = {}
+	
+	for (const [key, value] of Object.entries(tool.inputSchema.properties ?? {})) {
+		args[key] = await input({
+			message: `Enter value for ${key} (${(value as {type: string}).type}):`,
+		})
+	}
+
+	const res = await mcp.callTool({
+		name: tool.name,
+		arguments: args
+	})
+
+	console.log((res.content as [{text: string}])[0].text)
 }
 
 main()
