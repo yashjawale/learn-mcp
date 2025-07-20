2395efc84eda2f02d0f4c6f04db97f89b3d0f593
yashjawale <yash25.j@gmail.com>
2025-07-20 18:13:25 +0530

    feat: implement user creation functionality and add user data storage

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
+import z from 'zod'
+import fs from 'node:fs/promises'
 
 const server = new McpServer({
 	name: "Learn MCP Server",
@@ -10,6 +12,59 @@ const server = new McpServer({
 		prompts: {}
 	}
 })
+
+server.tool("create-user", "Create a new user in the database", {
+	name: z.string(),
+	email: z.string(),
+	address: z.string(),
+	phone: z.string()
+}, {
+	title: "Create User",
+	readOnlyHint: false,
+	destructiveHint: false,
+	idempotentHint: false,
+	openWorldHint: true
+}, async (params) => {
+	try {
+		const id = await createUser(params)
+		return {
+			content: [
+				{
+					type: "text",
+					text: `User ${id} created successfully`
+				}
+			]
+		}
+	} catch {
+		return {
+			content: [
+				{
+					type: "text",
+					text: "Failed to create user"
+				}
+			]
+		}
+	}
+})
+
+async function createUser(user: {
+	name: string,
+	email: string,
+	address: string,
+	phone: string
+}) {
+	const users = await import("./data/users.json", {
+		with: { type: "json" }
+	}).then(m => m.default)
+
+	const id = users.length + 1
+
+	users.push({ id, ...user })
+
+	fs.writeFile("./src/data/users.json", JSON.stringify(users, null, 2))
+
+	return id
+}
 
 async function main() {
 	const transport = new StdioServerTransport()
