2b5e9f3991e299aa8a291fc3164d1dfd64c1846e
yashjawale <yash25.j@gmail.com>
2025-07-20 20:49:46 +0530

    feat: add user details resource to retrieve specific user data from the database

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js"
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
 import z from 'zod'
 import fs from 'node:fs/promises'
@@ -23,14 +23,55 @@ server.resource(
 	}, async uri => {
 
 		const users = await import("./data/users.json", {
-		with: { type: "json" }
-	}).then(m => m.default)
+			with: { type: "json" }
+		}).then(m => m.default)
 
 		return {
 			contents: [
 				{
 					uri: uri.href,
 					text: JSON.stringify(users),
+					mimeType: "application/json"
+				}
+			]
+		}
+	}
+)
+
+server.resource(
+	"user-details",
+	new ResourceTemplate("users://{userId}/profile", {
+		list: undefined
+	}), 
+	{
+		description: "Get a user's details from the database",
+		title: "User Details",
+		mimeType: "application/json"
+	}, 
+	async (uri, { userId }) => {
+
+		const users = await import("./data/users.json", {
+			with: { type: "json" }
+		}).then(m => m.default)
+		const user = users.find(u => u.id === parseInt(userId as string))
+
+		if (user == null) {
+			return {
+				contents: [
+					{
+						uri: uri.href,
+						text: JSON.stringify({ error: "User not found" }),
+						mimeType: "application/json"
+					}
+				]
+			}
+		}
+
+		return {
+			contents: [
+				{
+					uri: uri.href,
+					text: JSON.stringify(user),
 					mimeType: "application/json"
 				}
 			]
