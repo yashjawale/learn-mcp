6598334ecf0d02b16751bb0eba3a1f9b5b3bd20b
yashjawale <yash25.j@gmail.com>
2025-07-20 23:29:40 +0530

    feat: add request handler for message processing

--- src/client.ts
+++ src/client.ts
@@ -3,16 +3,14 @@ import { createGoogleGenerativeAI } from '@ai-sdk/google';
 import { confirm, input, select } from '@inquirer/prompts';
 import { Client } from '@modelcontextprotocol/sdk/client/index.js';
 import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js';
-import { Prompt, PromptMessage, Tool } from '@modelcontextprotocol/sdk/types.js';
+import { CreateMessageRequestSchema, Prompt, PromptMessage, Tool } from '@modelcontextprotocol/sdk/types.js';
 import { generateText, jsonSchema, ToolSet } from 'ai';
 
 const mcp = new Client({
 	name: "Learn MCP Client",
 	version: "1.0.0",
-	capabilities: {
-		sampling: {},
-	}
-})
+},
+{ capabilities: { sampling: {} } })
 
 const transport = new StdioClientTransport({
 	command: "node",
@@ -32,6 +30,23 @@ async function main() {
 		mcp.listResources(),
 		mcp.listResourceTemplates()
 	])
+
+	mcp.setRequestHandler(CreateMessageRequestSchema, async request => {
+		const texts: string[] = []
+		for (const message of request.params.messages) {
+			const text = await handleServerMessagePrompt(message)
+			if (text != null) texts.push(text)
+		}
+		return {
+			role: "user",
+			model: "gemini-2.0-flash",
+			stopReason: "endTurn",
+			content: {
+				type: "text",
+				text: texts.join("\n")
+			}
+		}
+	})
 
 	console.log("You are connected")
 
