bc91a21f61f2520d89bcd756a4a361de052afe93
yashjawale <yash25.j@gmail.com>
2025-07-20 22:59:25 +0530

    feat: enhance resource selection and handling in main function

--- src/client.ts
+++ src/client.ts
@@ -41,21 +41,44 @@ async function main() {
 
 		switch (option) {
 			case "Tools":
-			const toolName = await select({
-				message: "Select a tool",
-				choices: tools.map(tool => ({
-					name: tool.annotations?.title || tool.name,
-					value: tool.name,
-					description: tool.description
-				}))
-			})
-			const tool = tools.find(t => t.name === toolName)
-			if (tool == null) {
-				console.error("Tool not found.")
-			} else {
-				await handleTool(tool)
-			}
-			break
+				const toolName = await select({
+					message: "Select a tool",
+					choices: tools.map(tool => ({
+						name: tool.annotations?.title || tool.name,
+						value: tool.name,
+						description: tool.description
+					}))
+				})
+				const tool = tools.find(t => t.name === toolName)
+				if (tool == null) {
+					console.error("Tool not found.")
+				} else {
+					await handleTool(tool)
+				}
+				break
+			case "Resources":
+				const resourceUri = await select({
+					message: "Select a resource",
+					choices: [
+						...resources.map(resource => ({
+							name: resource.name,
+							value: resource.uri,
+							description: resource.description
+						})),
+						...resourceTemplates.map(template => ({
+							name: template.name,
+							value: template.uriTemplate,
+							description: template.description
+						}))
+					]
+				})
+				const uri = resources.find(r => r.uri === resourceUri)?.uri || resourceTemplates.find(r => r.uriTemplate === resourceUri)?.uriTemplate
+				if (uri == null) {
+					console.error("Resource not found.")
+				} else {
+					await handleResource(uri)
+				}
+				break
 		}
 	}
 }
@@ -75,6 +98,29 @@ async function handleTool(tool: Tool) {
 	})
 
 	console.log((res.content as [{text: string}])[0].text)
+}
+
+async function handleResource(uri: string) {
+	let finalUri = uri
+
+	const paramMatches = uri.match(/{([^}]+)}/g)
+	
+	if (paramMatches !== null) {
+			for (const paramMatch of paramMatches) {
+			const paramName = paramMatch.replace("{", "").replace("}", "")
+			const paramValue = await input({
+				message: `Enter value for ${paramName}:`
+			})
+			finalUri = finalUri.replace(paramMatch, paramValue)
+		}
+	}
+
+
+	const res = await mcp.readResource({
+		uri: finalUri
+	})
+
+	console.log(JSON.stringify(JSON.parse(res.contents[0].text as string), null, 2))
 }
 
 main()
